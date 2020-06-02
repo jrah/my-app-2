cdacea3636b0b3a36adabcd0a86726fcc40fe1e9
James Home <jrah@users.noreply.github.com>
2020-06-02 15:04:11 +0100

    Fix vuejs instance

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    {{ message }}
-  </div>
+  <div id="app">{{ message }}</div>
 </template>
 
-<script type="text/javascript">
-var app = new Vue({
-  el: "#app",
-  data: {
-    message: "Hello Vue!"
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      message: "Hello Vue!"
+    };
   }
-});
+};
 </script>
 <style lang="scss"></style>
