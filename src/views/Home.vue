04cef36d56f7ea91ad778b818309a1fae91a98a6
hannah26hannah <ooyoonoo24@gmail.com>
2020-06-02 21:15:34 +0900

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <h1>Hosting Price</h1>
+    <h2>호스팅 비용 안내</h2>
+    <p>
+      Vue119는 CMS, Netlify 기반의 컨텐츠 관리 시스템을 적용하여 보다 편하게
+      홈페이지를 관리할 수 있도록 도와드립니다.
+    </p>
+    <table>
+      <tr>
+        <td v-for="type in types" :key="type">{{ type }}</td>
+      </tr>
+      <tr>
+        <td v-for="plan in plans" :key="plan">{{ plan }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
+<style scoped lang="scss">
+h1 {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 22px;
+}
+</style>
